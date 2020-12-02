a92ab7ca2c0a768bafa9d300b125f66da3b92e14
自由上网 <12132898+Alvin9999@users.noreply.github.com>
2020-12-02 22:47:04 +0800

    Update daze.sh

--- daze.sh
+++ daze.sh
@@ -110,7 +110,7 @@ Download(){
 	fi
 	wget --no-check-certificate -N "https://github.com/mohanson/daze/releases/download/${new_ver}/daze_linux_${bit}.zip"
 	[[ ! -e "daze_linux_${bit}.zip" ]] && echo -e "${Error} DAZE 下载失败 !" && rm -rf "${Folder}" && exit 1
-	mv "daze_linux_${bit}" "daze"
+	mv "daze_linux_${bit}.zip" "daze"
 	[[ ! -e "daze" ]] && echo -e "${Error} DAZE 重命名失败 !" && rm -rf "${Folder}" && exit 1
 	chmod +x daze
 	echo "${new_ver}" > ${Now_ver_File}
